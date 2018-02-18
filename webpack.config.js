31b061b0dfed9c8350c70252326d3544fc293426
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-02-18 11:21:42 -0800

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+  entry: './entry.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  node: {
+    // see http://webpack.github.io/docs/configuration.html#node
+    // and https://webpack.js.org/configuration/node/
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+};
